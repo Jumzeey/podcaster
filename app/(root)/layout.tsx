bd9cb99d5559b36916a3d0ec97afa328c59f44bc
Jumzeey <31732947+Jumzeey@users.noreply.github.com>
2024-09-01 16:19:02 +0100

    completed setup shadcn setup netlify setup git setup folder structure and layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,32 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="flex flex-col relative">
+            <main className="relative flex bg-black-3">
+                <LeftSidebar />
+
+                <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+                    <div className="flex w-full mx-auto max-w-5xl flex-col max-sm:px-4">
+                        <div className="flex h-16 justify-between items-center md:hidden">
+                            <Image src='/icons/logo.svg' width={30} height={30} alt="image"/>
+                            <MobileNav />
+                        </div>
+                        <div className="flex flex-col md:pb-14">
+                            Toaster (Notification)
+                            {children}
+                        </div>
+                    </div>
+                </section>
+                <RightSidebar />
+            </main >
+        </div>
+    );
+}
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import links from '@constants/sidebarLinks.json'
+import React from 'react'
+import { usePathname, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+const leftSidebar = () => {
+    const pathname = usePathname();
+    const router = useRouter()
+    return (
+        <section className='left_sidebar '>
+            <nav className='flex flex-col gap-6'>
+                <Link href="/" className='flex items-center cursor-pointer gap-1 pb-10 max-lg:justify-center'>
+                    <Image src="/icons/logo.svg" alt='logo' width={23} height={27} />
+                    <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">Podcaster</h1>
+                </Link>
+
+                {links.map(({ route, label, imgURL }) => {
+                    const isActive = pathname === route || pathname.startsWith(`${route}/`)
+
+                    return <Link
+                        href={route}
+                        key={label}
+                        className=
+                        {cn('flex gap-3 items-center py-4 justify-center max-lg:px-4 lg:justify-start',
+                            { 'bg-nav-focus border-r-4 border-orange-1': isActive })}>
+                        <Image src={imgURL} alt='labelIcon' width={24} height={24} />
+                        <p>{label}</p>
+                    </Link>
+                })}
+            </nav>
+        </section>
+    )
+}
+
+export default leftSidebar
